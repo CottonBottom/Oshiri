4680c3eaac70d3a41bbd812621a588dce2bdf770
CottonBottom <87255233+CottonBottom@users.noreply.github.com>
2022-03-19 08:13:45 +0900

    Cleanup

--- oshiri-front/src/Sandbox.test.tsx
+++ oshiri-front/src/Sandbox.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Sandbox from './sandbox/Sandbox';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Sandbox />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
