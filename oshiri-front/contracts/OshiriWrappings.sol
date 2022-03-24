a687c47bb9cd1276e38d478a912a2de5df34214e
CottonBottom <87255233+CottonBottom@users.noreply.github.com>
2022-03-24 20:40:50 +0900

    Making wrapping logic

--- oshiri-front/contracts/OshiriWrappings.sol
+++ oshiri-front/contracts/OshiriWrappings.sol
@@ -5,25 +5,72 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract OshiriWrappings is ERC721URIStorage{
+contract OshiriWrappings is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     //Incrementing value for each token
 
-    address contractAddress;
+    uint256 wTypeCurrent;
+    uint256 wSubTypeCurrent;
+    uint256 wVariationCurrent;
+    uint256 wBaseColorCurrent;
+    uint256 wVariationColorCurrent;
+    uint256 wSerialNumberCurrent;
 
-    constructor() ERC721("Oshiri Wrappings","OSHWRAP"){
+    address oshiriCurrencyAddress;
+
+    struct WrappingStats {
+        uint256 wType;
+        //1-6
+        uint256 wSubType;
+        //1-3
+        uint256 wVariation;
+        //1-4
+        uint256 wBaseColor;
+        //1-3
+        uint256 wVariationColor;
+        //1-6
+        uint256 wSerialNumber;
     }
 
-    // function createToken(string memory tokenURI) public returns (uint){
-    //     _tokenIds.increment();
-    //     uint256 newItemId = _tokenIds.current();
+    mapping(uint256 => mapping(uint256 => mapping(uint256 => mapping(uint256 => mapping(uint256 => uint256)))))
+        private CreatedWrappings;
 
-    //     _mint(msg.sender, newItemId);
-    //     _setTokenURI(newItemId, tokenURI);
-    //     setApprovalForAll(contractAddress, true);
-    //     //Gives the marketplace the approval to transact this token between users
-    //     //from within another contract
-    //     return newItemId;
-    // }
+    //wType => wSubType => wVariation => wBaseColor => wVariationColor => wSerialNumber
+
+    constructor() ERC721("Oshiri Wrappings", "OSHWRAP") {}
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        getNextInLine();
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        //Gives the marketplace the approval to transact this token between users
+        //from within another contract
+        return newItemId;
+    }
+
+    // returns wSerialNumberCurrent
+    function getNextInLine() private returns (WrappingStats memory) {
+        if (
+            CreatedWrappings[wTypeCurrent][wSubTypeCurrent][wVariationCurrent][
+                wBaseColorCurrent
+            ][wVariationColorCurrent] < 100
+        ) {
+            WrappingStats memory newWrapping = WrappingStats(
+                wTypeCurrent,
+                wSubTypeCurrent,
+                wVariationCurrent,
+                wBaseColorCurrent,
+                wVariationColorCurrent,
+                wSerialNumberCurrent
+            );
+            //Set first type += 1 if less than max, else go back to 0 and plus 1 to subType, do the same until colorCUrrent
+            return newWrapping;
+        } else {
+            //NFT Finished
+        }
+    }
+    //Should go through all, if no available: NFT ENDED
 }
